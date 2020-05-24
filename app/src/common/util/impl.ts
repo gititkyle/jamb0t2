de6db0354364c9d5675dbd6b3de2d681e83af3dc
chooooons <kyle.alan.hanson@gmail.com>
2020-05-23 23:13:03 -0700

    impl: log error

--- app/src/common/util/impl.ts
+++ app/src/common/util/impl.ts
@@ -1,5 +1,10 @@
-export function handleErrorResult (result: any) {
+import * as log4jambot from './logger';
+
+const logger = log4jambot.logger('impl');
+
+export function handleErrorResult (result: any): void {
     if(!Array.isArray(result)) {
+        logger.debug(`Error result: ${result}`);
         throw TypeError(`Invalid type ${typeof result}. Expected string[]`);
     }
 }
